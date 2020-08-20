7bcb8d914f16614ba1e2c9955ba62534bfb23abd
Ib Green <ib@unfolded.ai>
2020-08-20 07:14:02 -0700

    loader-utils: clean up ChildProcessProxy class

--- modules/loader-utils/src/lib/process-utils/child-process-proxy.d.ts
+++ modules/loader-utils/src/lib/process-utils/child-process-proxy.d.ts
@@ -1,35 +1,35 @@
 import ChildProcess from 'child_process';
 
 export type ProcessProxyOptions = {
+  /** Command to execute */
   command: string;
+  /** Arguments to the command */
   arguments: string[],
-  /** Whether to add a port specified arg */
-  portArg?: string,
-  /** Base port number */
-  port?: number,
-  /** Whether to search for an available port if the base port is occupied */
-  autoPort?: boolean,
+  /** If true emits helpful logging tracing the child process lifecycle */
+  debug?: boolean,
   /** Number of milliseconds to wait until concluding success */
   waitMs?: number,
   /** Options passed on to Node'.js `ChildProcess.spawn` */
   spawn?: ChildProcess.SpawnOptionsWithoutStdio,
-  /** Callback when the  */
+  /** Callback when the process has started */
   onStart?: (proxy: ChildProcessProxy) => void
 };
 
 /** 
- * Manager for a Node.js child process
+ * Manager for spawning, waiting for and terminating child process
  * Prepares arguments, starts, stops and tracks output
+ * Only supported under Node.js 
  */
 export default class ChildProcessProxy {
-  constructor(options?: {id?: string});
+  /** Whether ChildProcessProxy is supported on the current platform */
+  static isSupported(): boolean;
+
+  /** Create a new ChildProcessProxy instance */
+  constructor();
 
   /** Starts a child process with the provided options */
   start(options?: ProcessProxyOptions): Promise<object>;
 
   /** Stops a running child process */
   stop(): Promise<void>
-
-  /** Exits this process */
-  exit(statusCode?: number);
 }
